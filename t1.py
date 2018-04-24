e01a4355f9a89f028b707804f8cdf8eefc36afbd
xman12345 <118103170@qq.com>
2018-04-24 13:51:04 +0800

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,1 +1,42 @@
-1
+# -*- coding: utf-8 -*-
+
+import numpy
+import random
+
+e_End = 9
+e_Trap = 3
+e_Avatar = 1
+e_Disable = 8
+class XEvn:
+	weight = 10
+	height = 5
+	trapNum = 10
+	
+	def __init__(self):
+		self.data = numpy.zeros((self.weight,self.height))
+		randomVec = [i for i in range(self.weight*self.height)]
+		random.shuffle(randomVec)
+		#print(randomVec)
+		
+		index = randomVec.pop()
+		self.data[index/self.height][index%self.height] = e_End
+		index = randomVec.pop()
+		self.data[index/self.height][index%self.height] = e_Avatar
+		
+		for i in range(self.trapNum):
+			index = randomVec.pop()
+			self.data[index/self.height][index%self.height] = e_Disable
+		
+				
+	def show(self):
+		print(self.data)		
+
+
+evn = XEvn()
+evn.show()
+
+
+
+
+
+
